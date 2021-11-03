bd956ee088a5106ac3cd8e7fbfb1d8ed5c060ca5
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-03 19:50:37 +0530

    Create Order Rectification

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,11 +1,18 @@
 import Axios from 'axios';
 import { CART_EMPTY } from '../constants/cartConstants';
-import {  ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, } from '../constants/orderConstants';
+import {
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+} from '../constants/orderConstants';
 
 export const createOrder = (order) => async (dispatch, getState) => {
   dispatch({ type: ORDER_CREATE_REQUEST, payload: order });
   try {
-    const { userSignin: { userInfo }, } = getState();   //from user Sign in , we get the user info
+    const { userSignin: { userInfo },} = getState();   //from user Sign in , we get the user info
 
     const { data } = await Axios.post('/api/orders', order, {
       headers: {
@@ -26,28 +33,21 @@ export const createOrder = (order) => async (dispatch, getState) => {
   }
 };
 
-export const detailsOrder=(orderId)=>{
-  async(dispatch,getState)=>{
-    dispatch({type:ORDER_DETAILS_REQUEST,payload:orderId});
-    
-    const {userSignin:{userInfo},}=
-    getState();
-
-    try{
-const {data}=await Axios.get(`/api/orders/${orderId}`,
-{headers:{Authorization:`Bearer ${userInfo.token}`},
-});
-
-dispatch({type:ORDER_DETAILS_SUCCESS,payload:data});
-}
-    catch(error){
-      const message=
-      error.message&&error.response.data.message?
-      error.response.data.message:
-      error.message;           // either use in built error message or the general error message (n/w error)
-  
-      dispatch({type:ORDER_DETAILS_FAIL,payload:messge});
-    }
-    
-  };
-}
+export const detailsOrder = (orderId) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId });
+  const {
+    userSignin: { userInfo },
+  } = getState();
+  try {
+    const { data } = await Axios.get(`/api/orders/${orderId}`, {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;      // either use in built error message or the general error message (n/w error)
+    dispatch({ type: ORDER_DETAILS_FAIL, payload: message });
+  }
+};
