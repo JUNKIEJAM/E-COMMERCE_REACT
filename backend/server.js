38c1483ee9528a597a03096215861416d64ec354
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-07 15:39:19 +0530

    Connecting backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
-import path from 'path';
 
 dotenv.config();
 
@@ -25,10 +25,15 @@ app.get('/api/config/paypal',(req,res)=>{
   res.send(process.env.PAYPAL_CLIENT_ID||'sb'); //sb=sandbox
 });
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
-// });
+/* app.get('/', (req, res) => {
+   res.send('Server is ready');
+ });*/
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
